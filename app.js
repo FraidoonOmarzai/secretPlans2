d1e1b0c378ba9c35f9aecbc763d8163ca90b6849
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2020-12-14 10:02:51 +0530

    go ahead and follow ur dreams :)

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ app.post("/login", (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 app.listen(port,()=>{
    console.log('Server is started successfully!');
